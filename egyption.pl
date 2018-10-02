edc32a540ccc9c7be582926bf3ac53438e1c9b92
Oscar Meanwell <oscar.meanwell@gmail.com>
2018-10-02 16:15:39 +0100

    Create egyption.pl

---
+++ egyption.pl
@@ -0,0 +1,37 @@
+use strict;
+use warnings;
+use bigint;
+sub isEgyption{
+    my $nr = int($_[0]);
+    my $de = int($_[1]);
+    if($nr == 0 or $de == 0){
+        #Invalid input
+        return;
+    }
+    if($de % $nr == 0){
+        # They divide so print
+        printf "1/" . int($de/$nr);
+        return;
+    }
+    if($nr % $de == 0){
+        # Invalid fraction
+        printf $nr/$de;
+        return;
+    }
+    if($nr > $de){
+        printf $nr/$de . " + ";
+        isEgyption($nr/$de, $de);
+    }
+    # Floor to find ceiling and print as fraction
+    my $tmp = int($de/$nr) + 1;
+    printf "1/" . $tmp . " + ";
+    isEgyption($nr*$tmp-$de, $de*$tmp);
+}
+
+printf "Please input Numerator: ";
+my $nrI = int(<>);
+printf "Please input Denominator: ";
+my $deI = int(<>);
+printf "\nEgyptian Fraction Representation of " . $nrI . "/" . $deI . " is: \n\n";
+isEgyption($nrI,$deI);
+printf "\n";
